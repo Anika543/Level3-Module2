0c27e895ac7e1143ef47e8a33f1d77642778efaa
league <maddoxseul@sbsd.net>
2026-06-20 11:30:13 -0700

    Working on more algorithms, finding the longest string

--- src/_02_Intro_To_Searching_Algorithms/_03_SearchTest.java
+++ src/_02_Intro_To_Searching_Algorithms/_03_SearchTest.java
@@ -9,17 +9,22 @@ class _03_SearchTest {
     /*
      *  A minimum of 3 tests are required for each method
      */
+	String[] x = {"sdf", "hello", "abc", "dksdfsd"};
+	String value = "abc"; 
 
+	int[] arr1 = {1, 3, 4, 8, 15, 24, 233}; 
+	int value2 = 24; 
+	
     @Test
     public void testLinearSearch() {
         // 1. Use the assertEquals() method to test your linear search method
-        assertEquals(false, true);
+        assertEquals(2, _01_LinearSearch.linearSearch(x, value));
     }
 
     @Test
     public void testBinarySearch() {
         // 2. Use the assertEquals() method to test your binary search method
         //    remember that the array must be sorted
-        assertEquals(false, true);
+        assertEquals(5, _02_BinarySearch.binarySearch(arr1, 0, arr1.length-1, value2));
     }
 }
--- src/_03_More_Algorithms/Algorithms.java
+++ src/_03_More_Algorithms/Algorithms.java
@@ -6,12 +6,120 @@ import java.util.List;
  * Write your methods in this class
  */
 public class Algorithms {
-    
-    public static String multiply(int num1, int num2) {
-        return null;
-    }
-    
-    public static int findBrokenEgg(List<String> eggs) {
-        return 0;
-    }
+
+	public static String multiply(int num1, int num2) {
+
+		int y = num1 * num2;
+		String x = num1 + " x " + num2 + " = " + y;
+
+		return x;
+	}
+
+	public static boolean isPrime(int num) {
+		boolean prime = true;
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0) {
+				prime = false;
+			}
+		}
+
+		return prime;
+	}
+
+	public static boolean isSquare(int num) {
+		boolean square = false;
+
+		for (int i = 0; i < num+1; i++) {
+			if (i * i == num) {
+				square = true;
+			}
+		}
+
+		return square;
+	}
+	
+	public static boolean isCube(int num) {
+		boolean cube = false; 
+		
+		for (int i = 0; i < num+1; i++) {
+			if (i * i * i == num) {
+				cube = true;
+			}
+		}
+		
+		return cube; 
+	}
+
+	public static int findBrokenEgg(List<String> eggs) {
+		int index = -1; 
+		for(int i = 0; i<eggs.size(); i++) {
+			if(eggs.get(i).equals("cracked")) {
+				index = i; 
+				break; 
+			}
+		}
+		return index; 
+	}
+	
+	public static int countPearls(List<Boolean> pearls) {
+		int count = 0; 
+		for(int i = 0; i<pearls.size(); i++) {
+			if(pearls.get(i) == true) {
+				count++; 
+			}
+		}
+		
+		return count; 
+	}
+	
+	public static double findTallest(List<Double> peeps) {
+		Double x = 0.0; 
+		
+		for(int i = 0; i<peeps.size(); i++) {
+			if(peeps.get(i) > x) {
+				x = peeps.get(i); 
+			}
+		}
+		
+		return x; 
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
